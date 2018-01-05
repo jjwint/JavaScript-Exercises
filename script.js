1620d1713252671ad87d67c1f65c2ba94f7ec079
Jacqueline Winterbottom <jjwint90@gmail.com>
2018-01-05 17:56:33 -0500

    Exercise 1

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-//Exercise 4
+//Exercise 1
+var num = prompt("Enter a number.");
+var total = 0;
 
+for (var i=0; i<=num; i++) {
+	total = total + i;
+}
+
+console.log(total);
+
+//Exercise 4
 var timeOfDay = prompt("Is it morning, noon, or evening?");
 
 if (timeOfDay === "morning") {
